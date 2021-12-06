f8653aa4f3067dc4ea3cc6e40963dece679332b6
Paul-kav <paulkavuma82@gmail.com>
2021-12-06 10:35:47 -0500

    Handling cross platform environments and file systems

---
+++ WorkingWithFileSystems/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace WorkingWithFileSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputFileSystemInfo();
+            //Console.WriteLine("Hello World!");
+        }
+
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0, -33} {1}", arg0: "Path.PathSeparator", arg1: PathSeparator);
+            WriteLine("{0, -33} {1}", arg0: "Path.DirectorySeparatorChar", arg1: DirectorySeparatorChar);
+            WriteLine("{0, -33} {1}", arg0: "Directory.GetCurrentDirectory()", arg1: GetCurrentDirectory());
+            WriteLine("{0, -33} {1}", arg0: "Environment.CurrentDirectory", arg1: CurrentDirectory);
+            WriteLine("{0, -33} {1}", arg0: "Environment.SystemDirectory", arg1: SystemDirectory);
+            WriteLine("{0, -33} {1}", arg0: "Path.GetTempPath()", arg1: GetTempPath());
+
+            WriteLine("GetFolderPath(SpecialFolder");
+            WriteLine("{0, -33} {1}", arg0: " .System)",
+                arg1: GetFolderPath(SpecialFolder.System));
+            WriteLine("{0, -33} {1}", arg0: " .ApplicationData)",
+                arg1: GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0, -33} {1}", arg0: " .MyDocuments)",
+                arg1: GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0, -33} {1}", arg0: " .Personal)",
+                arg1: GetFolderPath(SpecialFolder.Personal));
+        }
+    }
+
+
+}
