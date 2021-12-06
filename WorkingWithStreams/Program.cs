1c37d23a666e0d0d398f0d9eb8a3e69c29a05a6b
Paul-kav <paulkavuma82@gmail.com>
2021-12-06 16:10:58 -0500

    Writing to xml streams

--- WorkingWithStreams/Program.cs
+++ WorkingWithStreams/Program.cs
@@ -11,9 +11,10 @@ namespace WorkingWithStreams
     {
         static void Main(string[] args)
         {
-            WorkWithText();
+            //WorkWithText();
+            WorkWithXml();
         }
-
+        //Writing to text streams
         static void WorkWithText()
         {
             //define a file to write to
@@ -36,6 +37,47 @@ namespace WorkingWithStreams
                 arg1: new FileInfo(textFile).Length);
 
             WriteLine(File.ReadAllText(textFile));
+        }
+
+        //Writing to xml streams
+        static void WorkWithXml()
+        {
+            //define a file to write to
+            string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+            //create a file stream
+            FileStream xmlFileStream = File.Create(xmlFile);
+
+            //wrap the file stream in an XML writer helper
+            //and automatically indent nested elements
+            XmlWriter xml = XmlWriter.Create(xmlFileStream,
+                new XmlWriterSettings { Indent = true });
+
+            //write the Xml declaration
+            xml.WriteStartDocument();
+
+            //write a root element
+            xml.WriteStartElement("callsigns");
+
+            //enumerate the strings writing each one to the stream
+            foreach (string item in Viper.Callsigns)
+            {
+                xml.WriteElementString("callsign", item);
+            }
+
+            //write the close root element
+            xml.WriteEndElement();
+
+            //close helper and stream
+            xml.Close();
+            xmlFileStream.Close();
+
+            //output all the contents of the file
+            WriteLine("{0} contains {1:NO} bytes.",
+                arg0: xmlFile,
+                arg1: new FileInfo(xmlFile).Length);
+            WriteLine(File.ReadAllText(xmlFile));
+
         }
 
         static class Viper
